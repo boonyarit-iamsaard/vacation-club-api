880b79a8e1600f2b4696bdb5b97affc0c2991822
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2024-02-24 19:34:04 +0700

    build: add database and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_USER: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_HOST: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/database.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule,
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const user = configService.get<string>('DATABASE_USER');
+        const password = configService.get<string>('DATABASE_PASSWORD');
+        const name = configService.get<string>('DATABASE_NAME');
+        const host = configService.get<string>('DATABASE_HOST');
+
+        return {
+          uri: `mongodb+srv://${user}:${password}@${host}/?retryWrites=true&w=majority&appName=${name}`,
+        };
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
