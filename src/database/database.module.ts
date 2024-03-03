5e1e25e43020fad0801d376031ef19dd01e298a4
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2024-03-03 10:00:41 +0700

    build: add prisma

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
   imports: [
     ConfigModule,
+    PrismaModule.forRootAsync({
+      inject: [ConfigService],
+      isGlobal: true,
+      useFactory: async (configService: ConfigService) => ({
+        explicitConnect: false,
+        prismaOptions: {
+          datasources: {
+            db: {
+              url: configService.get<string>('DATABASE_URL'),
+            },
+          },
+          log: ['info', 'query'],
+        },
+      }),
+    }),
     MongooseModule.forRootAsync({
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
