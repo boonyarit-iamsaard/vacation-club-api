6956fa64fea2eb6abb7443d8c20cc1a43c2723ce
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2024-03-09 21:01:06 +0700

    build: add class serializer interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
-import { ValidationPipe, VersioningType } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import {
+  ClassSerializerInterceptor,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix('api');
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: 'v1',
   });
+  app.setGlobalPrefix('api');
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 }
