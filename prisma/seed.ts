c2827a87ec507070c86ad6bb186b2e4d1330d4ea
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2024-03-18 20:42:22 +0700

    build: add seed data

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client';
+import * as argon2 from 'argon2';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = {
+    email: 'arya@stark.com',
+    password: await argon2.hash('winteriscoming'),
+    name: 'Aray Stark',
+  };
+  const { email } = user;
+
+  await prisma.user.upsert({
+    where: { email },
+    update: user,
+    create: user,
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
